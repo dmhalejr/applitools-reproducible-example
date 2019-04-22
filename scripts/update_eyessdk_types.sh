83e1e68c442127904f3e3d76abb28766099fbf7f
David Hale <david.hale@lonelyplanet.com>
2019-04-22 14:13:49 -0500

    fix: actually give types folder

--- scripts/update_eyessdk_types.sh
+++ scripts/update_eyessdk_types.sh
@@ -2,7 +2,7 @@
 
 # This should be removed once this is merged: https://github.com/applitools/Eyes.Sdk.JavaScript/pull/71
 EYES_SDK_OLD_TYPES="./node_modules/eyes.sdk/typings/index.d.ts"
-EYES_SDK_NEW_TYPES="./scripts/typings/eyes.sdk/index.d.ts"
+EYES_SDK_NEW_TYPES="./scripts/types/eyes.sdk/index.d.ts"
 
 if cmp -s "$EYES_SDK_OLD_TYPES" "$EYES_SDK_NEW_TYPES" ; then
   echo "Type does not need to update"
@@ -15,7 +15,7 @@ else
 fi
 
 EYES_SELENIUM_OLD_TYPES="./node_modules/eyes.selenium/typings/index.d.ts"
-EYES_SELENIUM_NEW_TYPES="./scripts/typings/eyes.selenium/index.d.ts"
+EYES_SELENIUM_NEW_TYPES="./scripts/types/eyes.selenium/index.d.ts"
 
 if cmp -s "$EYES_SELENIUM_OLD_TYPES" "$EYES_SELENIUM_NEW_TYPES" ; then
   echo "Type does not need to update"
